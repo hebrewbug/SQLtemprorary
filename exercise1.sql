13540b68766cd0ecb409cb637e0c73afad58f8f9
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-05-11 23:20:39 +0300

    Update exercise1.sql

--- exercise1.sql
+++ exercise1.sql
@@ -7,5 +7,16 @@ SELECT DISTINCT c.name, total_investment, imports
           FROM languages AS l
           WHERE official = 'true'
         ) )
-  WHERE region = 'Central America' AND year = 2015
+  WHERE region = 'Central America' AND year = 2002
 ORDER BY c.name;
+
+
+
+
+SELECT region, continent, AVG(p.fertility_rate) AS avg_fert_rate
+  FROM countries AS c
+    INNER JOIN populations AS p
+      ON c.code = p.country_code
+  WHERE year = 2019
+GROUP BY region, continent
+ORDER BY avg_fert_rate;
