b1e21b405a41323fd8c2e13ce9b110bce9339274
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-05-11 23:29:26 +0300

    Update and rename exercise1.sql to query.sql

--- query.sql
+++ query.sql
@@ -20,3 +20,23 @@ SELECT region, continent, AVG(p.fertility_rate) AS avg_fert_rate
   WHERE year = 2019
 GROUP BY region, continent
 ORDER BY avg_fert_rate;
+
+
+-- Select fields
+SELECT name, country_code, city_proper_pop, metroarea_pop,  
+      -- Calculate city_perc
+      metroarea_pop / city_proper_pop * 100 AS city_perc
+  -- From appropriate table
+  FROM cities
+  -- Where 
+  WHERE name IN
+    -- Subquery
+    (SELECT capital
+     FROM countries
+     WHERE (region = 'Europe'
+        OR region LIKE '%America'))
+       AND metroarea_pop IS NOT NULL
+-- Order appropriately
+ORDER BY city_perc DESC
+-- Limit amount
+LIMIT 11;
