c1dc3fad2ebcb540d89853282acd103031fb2529
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-05-31 05:19:49 +0300

    subqueries

--- query.sql
+++ query.sql
@@ -75,3 +75,19 @@ FROM
     ON m.country_id = c.id) AS subq
 -- Filter by total goals scored in the main query
 WHERE total_goals >= 10;
+
+--Nested simple subqueries
+
+SELECT
+	-- Select the season and max goals scored in a match
+	season,
+    MAX(home_goal + away_goal) AS max_goals,
+    -- Select the overall max goals scored in a match
+   (SELECT MAX(home_goal + away_goal) FROM match) AS overall_max_goals,
+   -- Select the max number of goals scored in any match in July
+   (SELECT MAX(home_goal + away_goal) 
+    FROM match
+    WHERE id IN (
+          SELECT id FROM match WHERE EXTRACT(MONTH FROM date) = 07)) AS july_max_goals
+FROM match
+GROUP BY season;
