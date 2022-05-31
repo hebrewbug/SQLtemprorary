1598612c2814583aec6df40669e23ad0ae690452
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2022-05-31 05:49:23 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -91,3 +91,22 @@ SELECT
           SELECT id FROM match WHERE EXTRACT(MONTH FROM date) = 07)) AS july_max_goals
 FROM match
 GROUP BY season;
+
+
+--This one is wonderfull 
+-- Set up your CTE
+WITH match_list AS (
+  -- Select the league, date, home, and away goals
+    SELECT 
+  		l.name AS league, 
+     	m.date, 
+  		m.home_goal, 
+  		m.away_goal,
+       (m.home_goal + m.away_goal) AS total_goals
+    FROM match AS m
+    LEFT JOIN league as l ON m.country_id = l.id)
+-- Select the league, date, home, and away goals from the CTE
+SELECT league, date, home_goal, away_goal
+FROM match_list
+-- Filter by total goals
+WHERE total_goals > 9;
